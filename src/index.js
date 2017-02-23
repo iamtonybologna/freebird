f49356815e29136558b4ea34feeef5144fabbd48
Anthony <heyzeus@GALAXY.local>
2017-02-23 10:53:36 -0500

    removed merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import './index.css';
 import {
@@ -25,5 +24,3 @@ ReactDOM.render((
 //     <Route path="/users" component={Users} />
 //   </Router>
 // ), document.getElementById('users'));
-
-injectTapEventPlugin();
