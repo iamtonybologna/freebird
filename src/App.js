ccea1a6fac181101e5c3e0449488bf79e36d8728
David Jeffrey <dsejeffrey@gmail.com>
2017-02-23 14:14:37 -0500

    removed old files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 import {deepOrange500} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -20,54 +19,15 @@ class App extends Component {
       open: false,
     };
   }
-
-
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <Host/>
-=======
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Link } from 'react-router-dom';
-import io from 'socket.io-client';
-
-class App extends Component {
-
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    this.ws = io.connect('ws://localhost:4000');
-    this.ws.emit('event', function(data) {
-      console.log("Hello");
-    });
-  };
-
-  componentWillUnmount() {
-    console.log('Closing socket connection');
-    this.ws.close();
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
->>>>>>> 86711b17071889c864398c83c592597f9d8cf65f
         </div>
-        <p className="App-intro">
-          HOSTS PAGE
-        </p>
-        <p>
-          <Link to="/host">Host Page</Link>
-          <br/>
-          <Link to="/users">Users Page</Link>
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
-};
+}
 
 export default App;
