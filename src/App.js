a5206d52ffc043f6f6a85ae0631d02814c8b31ff
drakaven <drakaven@hotmail.com>
2017-02-24 19:24:03 -0500

    added player component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import {Link} from 'react-router-dom';
 import io from 'socket.io-client';
-
+import Player from './Player.js';
 
 class App extends Component {
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player1Hidden: "block",
+      player2Hidden: "none"
+    };
+  }
+
+
+    componentDidMount() {
     console.log("componentDidMount <App />");
     this.ws = io.connect('ws://localhost:4000');
     this.ws.emit('youTube', 'ok');
@@ -18,7 +27,6 @@ class App extends Component {
 
   }
 
-  //recusive timer that test for YT load
   startUp() {
     let err = false;
     try {
@@ -27,28 +35,38 @@ class App extends Component {
       console.log(err);
       err = true;
     }
-    if (err === true){
-     setTimeout(() => {
+    if (err === true) {
+      setTimeout(() => {
         this.startUp();
       }, 500);
-
-
     } else {
       console.log('loaded');
       window.player1.playVideo();
+      this.playerTime();
     }
   };
 
-  loadPlayer() {
-    console.log('clicked');
-    new window.YT.Player('player3', {
-      height: '390',
-      width: '640',
-      videoId: 'bnKFaAS30X8'
-    });
-    window.player1.stopVideo();
+  playerTime() {
+    let videoDuration = window.player1.getDuration();
+    let timePlayed = window.player1.getCurrentTime();
+    if (videoDuration - timePlayed <= 1000)
+    {
+      this.loadPlayer();
+    } else {
+      setTimeout(() => {
+        this.playerTime();
+        console.log('nope');
+      }, 1000);
+    }
   }
 
+  loadPlayer() {
+    console.log('clicked');
+    window.player2.setVolume(100);
+    window.player2.playVideo();
+    this.setState({player1Hidden: "none"});
+    this.setState({player2Hidden: "block"});
+  }
 
   componentWillUnmount() {
     console.log('Closing socket connection');
@@ -69,12 +87,14 @@ class App extends Component {
           <Link to="/host">Host Page</Link>
           <br/>
           <Link to="/users">Users Page</Link>
-          <button onClick={this.loadPlayer}>Movie</button>
+          <button onClick={this.loadPlayer}></button>
         </p>
-
-        <div id="player1"></div>
-        <div id="player2"></div>
-
+        <div class="wrapper" style={{display: this.state.player1Hidden}}>
+        <Player id={"player1"} ></Player>
+        </div>
+        <div class="wrapper" style={{display: this.state.player2Hidden}}>
+          <Player id={"player2"} ></Player>
+        </div>
       </div>
     );
   }
---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Player extends Component {
+
+  render() {
+    return (
+      <div id={this.props.id}>
+      </div>
+    );
+  }
+}
+export default Player;
