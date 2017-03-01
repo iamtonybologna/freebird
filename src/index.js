06d9190e0ed5ebfe16ad4c9b699e2ebf251dd88c
Anthony <heyzeus@GALAXY.local>
2017-03-01 18:12:40 -0500

    test

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import App from './App';
 import Users from './Users';
 import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 injectTapEventPlugin();
 
-ReactDOM.render((
+ReactDOM.render(
   <Router>
     <div>
+      <Route
+        path='/'
+        render={() => <Redirect to={{ pathname: '/host' }} />}
+      />
       <Route path='/host' component={App} />
       <Route path='/users' component={Users} />
     </div>
   </Router>
-), document.getElementById('root'));
+, document.getElementById('root'));
