bcde98344bb2712e7905c6a1454ee488bc071d13
David Jeffrey <dsejeffrey@gmail.com>
2017-03-02 12:02:43 -0500

    now working witg webpack again

--- src/splash.js
+++ src/splash.js
@@ -18,6 +18,7 @@ export default class Splash extends Component {
         primary={true}
         style={styles.button}
         onTouchTap={() => this.select('loading')} />
+      <div>helo</div>
       </div>
     )
   };
