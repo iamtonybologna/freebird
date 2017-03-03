2ad8e7d6f05a7969cdeeb868665b4238af8de238
drakaven <drakaven@hotmail.com>
2017-03-02 19:57:56 -0500

    module integration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Users from './Users';
+import Three from './Three';
 import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -12,6 +13,7 @@ ReactDOM.render((
   <Router>
     <div>
       <Route path='/host' component={App} />
+      <Route path='/three' component={Three} />
       <Route path='/users' component={Users} />
     </div>
   </Router>
