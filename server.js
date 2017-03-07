98347a72c32fd2859cad5d8f28e276609047f08a
Anthony <heyzeus@GALAXY.local>
2017-03-06 23:42:50 -0500

    small fix

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ io.on('connection', (client) => {
       console.log('song found in playlist');
       if (newSong.uploader === songInPlaylistUploader) {
         console.log('uploader id matches song in playlist uploader id');
-        array.splice(songInPlaylistArrayPosition, 1);
+        playlist.splice(songInPlaylistArrayPosition, 1);
         io.emit('updatePlaylist', { data: playlist });
       }
     };
